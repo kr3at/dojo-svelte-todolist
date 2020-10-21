8ba49555152972b6c4045d174e96583693dc1710
Wagner <wagner@tinkin.one>
2020-10-21 18:03:19 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+## DOJO SVELTE
+
+Create a basic authenticated TODO app with Svelte and Firebase
+* login/logout with google Oauth
+* create, querey, update, delete associated todo items
+
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyB4HaucnyQaIDhD_8noD9Y9FANb7eYv_Co",
+    authDomain: "todo-list-a9af0.firebaseapp.com",
+    databaseURL: "https://todo-list-a9af0.firebaseio.com",
+    projectId: "todo-list-a9af0",
+    storageBucket: "todo-list-a9af0.appspot.com",
+    messagingSenderId: "852836821423",
+    appId: "1:852836821423:web:9c10f80513fe1d34afe75a"
+  };
